81da6e22c697b70835c2c84769036b8d6e17ad02
Luan Santos <luanevangelistads@gmail.com>
2024-12-20 11:25:12 -0300

    Conclusão da função de conversão de tempo.

--- Conversor.c
+++ Conversor.c
@@ -9,7 +9,7 @@
 // void convesao_velocidade();
 // void convesao_energia();
 // void convesao_area();
-// void convesao_tempo();
+void convesao_tempo();
 // void convesao_ArmDados();
 
 int main()
@@ -35,30 +35,30 @@ int main()
 
     switch (loop)
     {
-      // case 1:
-      //   convesao_comprimento();
-      //   break;
-      // case 2:
-      //   convesao_massa();
-      //   break;
-      // case 3:
-      //   convesao_volume();
-      //   break;
-      // case 4:
-      //   convesao_temperatura();
-      //   break;
-      // case 5:
-      //   convesao_velocidade();
-      //   break;
-      // case 6:
-      //   convesao_energia();
-      //   break;
-      // case 7:
-      //   convesao_area();
-      //   break;
-      // case 8:
-      //   convesao_tempo();
-      //   break;
+    // case 1:
+    //   convesao_comprimento();
+    //   break;
+    // case 2:
+    //   convesao_massa();
+    //   break;
+    // case 3:
+    //   convesao_volume();
+    //   break;
+    // case 4:
+    //   convesao_temperatura();
+    //   break;
+    // case 5:
+    //   convesao_velocidade();
+    //   break;
+    // case 6:
+    //   convesao_energia();
+    //   break;
+    // case 7:
+    //   convesao_area();
+    //   break;
+    case 8:
+      convesao_tempo();
+      break;
       // case 9:
       //   convesao_ArmDados();
       //   break;
@@ -68,6 +68,45 @@ int main()
     default:
       printf("Opção inválida. Tente novamente.\n");
     }
+    system("Pause");
+    system("cls");
   }
   return 0;
+}
+
+//-------------------> 8. Unidades de tempo (segundos, minutos, horas).
+void convesao_tempo()
+{
+
+  int time;
+
+  printf("Qual a unidade de tempo que você possui para converter? \n ");
+  printf("1 - Horas \n 2 - Minutos \n 3 - Segundos \n");
+  scanf("%d", &time);
+
+  if (time == 1)
+  {
+    printf("Hora: ");
+    scanf("%i", &time);
+    printf("%i Minutos \n", time * 60);
+    printf("%i Segundos \n", (time * 60) * 60);
+  }
+  else if (time == 2)
+  {
+    printf("Minutos: ");
+    scanf("%i", &time);
+    printf("%i Horas \n", time / 60);
+    printf("%i Segundos \n", time * 60);
+  }
+  else if (time == 3)
+  {
+    printf("Segundos: ");
+    scanf("%i", &time);
+    printf("%i Horas \n", (time / 60) / 60);
+    printf("%i Minutos \n", time / 60);
+  }
+  else
+  {
+    printf("Valor incorreto, retorne ao menu para selecionar as opções! \n");
+  }
 }
